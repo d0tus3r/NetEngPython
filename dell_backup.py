87f549fe12efd95957a72a5e7358889f992f2d19
Michael "Irish" O'Neill <irish.dot@gmail.com>
2018-04-11 22:28:37 -0400

    Conform to some more conventional Python standards
    - Rename `DellBackup.py` to 'dell_backup.py`.
    - Add shebang line.
    - Convert comments at top of file to `docstring`.
    - Fixup whitespace.

--- dell_backup.py
+++ dell_backup.py
@@ -1,35 +1,38 @@
-#Dell PowerConnect 3xxx5xxx series auto backup script
-#parses file 'dellSwitches' for IPs to telnet and copy backup configuration
+#!/usr/bin/env python3
 
-#import necessary libraries
+''' Dell PowerConnect 3xxx5xxx series auto backup script parses file
+'dellSwitches' for IPs to telnet and copy backup configuration '''
+
+# import necessary libraries
 import getpass
-import sys
 import telnetlib
 import time
 import datetime
 
 
-#setup username and password creds for telnet session
-user = raw_input("Enter username for telnet session: ")
+# setup username and password creds for telnet session
+user = input("Enter username for telnet session: ")
 password = getpass.getpass()
 now = datetime.datetime.now()
 timestamp = (str(now.month) + "-" + str(now.day) + "-" + str(now.year))
 
-#pass login credentials to telnet session
+
+# pass login credentials to telnet session
 def dellLogin():
     tn.read_until("User Name:")
     tn.write(user + "\n")
     tn.read_until("Password:")
     tn.write(str(password) + "\n")
 
-#set datadump mode to remove need for user input on bulk data dump
-#dump runningcfg and save to file
+
+# set datadump mode to remove need for user input on bulk data dump
+# dump runningcfg and save to file
 def dellGetBackup():
     tn.read_until("#")
     tn.write("terminal datadump\n")
     tn.read_until("#")
     tn.write("show running-config\n")
-    #sleep after show running config for command to finish
+    # sleep after show running config for command to finish
     time.sleep(10)
     configOutput = tn.read_very_eager()
     backupConfig = open(str(host) + "-" + timestamp, "w")
@@ -37,17 +40,17 @@ def dellGetBackup():
     backupConfig.close()
     tn.write("exit\n")
 
-#file path to dellSwitches
+
+# file path to dellSwitches
 f = open("dellSwitches")
-#main loop - run two functions on each IP found in dellSwitches file
+# main loop - run two functions on each IP found in dellSwitches file
 for line in f:
-    print "Connecting to switch " + str(line)
+    print("Connecting to switch {}", str(line))
     host = line.strip()
-    
+
     tn = telnetlib.Telnet(host)
-     
+
     dellLogin()
     dellGetBackup()
 
 f.close()
-
