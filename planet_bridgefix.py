01cbf25be00169c22f3c7796734bee447ae5164d
d0tus3r <d0tus3r@gmail.com>
2018-04-23 14:49:04 -0400

    fixes formatting of string input before converting to byte stream

--- planet_bridgefix.py
+++ planet_bridgefix.py
@@ -10,11 +10,11 @@ from config import CONFIG
 USER = CONFIG.get('PLANET', 'USERNAME')
 PASSWORD = CONFIG.get('PLANET', 'PASSWORD')
 HOST = input('Enter PLANET Dslam IP: ')
-PORT = input('Enter the port')
-BRIDGE_NUMBER = input('Enter bridge port to reset')
-PVC = input('Enter PVC port')
-VLAN = input('Enter native vlan')
-SERV_PROFILE = input('Enter service profile number')
+PORT = input('Enter the port: ')
+BRIDGE_NUMBER = input('Enter bridge port to reset: ')
+PVC = input('Enter PVC port: ')
+VLAN = input('Enter native vlan: ')
+SERV_PROFILE = input('Enter service profile number: ')
 
 
 def planet_login():
@@ -31,19 +31,23 @@ def planet_login():
 def planet_bridge_reset():
     ''' commands related to bridge reset - needs port, vlan, pvc #'''
     TN.read_until(b'IDL-2402:(conf)#')
-    TN.write(b'interface xdsl {}'.format(PORT))
+    str_in = 'interface xdsl {}'.format(PORT)
+    TN.write(str_in.encode('ascii') + b'\n')
     time.sleep(2)
     TN.read_until(b'IDL-2402:(intf-conf)#')
-    TN.write(b'bridge {} disable'.format(BRIDGE_NUMBER))
+    str_in = 'bridge {} disable'.format(BRIDGE_NUMBER)
+    TN.write(str_in.encode('ascii') + b'\n')
     time.sleep(2)
     TN.read_until(b'IDL-2402:(intf-conf)#')
-    TN.write(b'bridge {} pvc 0/{}'.format(BRIDGE_NUMBER, PVC))
+    str_in = 'bridge {} pvc 0/{}'.format(BRIDGE_NUMBER, PVC)
+    TN.write(str_in.encode('ascii') + b'\n')
     time.sleep(2)
     TN.read_until(b'IDL-2402:(bridge-atm-conf)#')
     TN.write(b'pvc encapsulation auto')
     time.sleep(2)
     TN.read_until(b'IDL-2402:(bridge-atm-conf)#')
-    TN.write(b'default vlan {}'.format(VLAN))
+    str_in = 'default vlan {}'.format(VLAN)
+    TN.write(str_in.encode('ascii') + b'\n')
     time.sleep(2)
     TN.read_until(b'IDL-2402:(bridge-atm-conf)#')
     TN.write(b'exit')
@@ -52,7 +56,8 @@ def planet_bridge_reset():
     TN.write(b'adsl-config')
     time.sleep(2)
     TN.read_until(b'IDL-2402:(adsl-intf-conf)#')
-    TN.write(b'line profile service {}'.format(SERV_PROFILE))
+    str_in = 'line profile service {}'.format(SERV_PROFILE)
+    TN.write(str_in.encode('ascii') + b'\n')
     time.sleep(2)
     TN.read_until(b'IDL-2402:(adsl-intf-conf)#')
     TN.write(b'line status service reset')
@@ -75,5 +80,8 @@ def planet_bridge_reset():
 
 
 
+
 TN = telnetlib.Telnet(HOST)
 planet_login()
+planet_bridge_reset()
+time.sleep(10)
